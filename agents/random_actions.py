d247443b1e947bee9eb138dc1179594262c988d9
JarvisEQ <lfjarvis@gmail.com>
2020-03-01 17:51:04 -0500

    adding random agent

--- agents/random_actions.py
+++ agents/random_actions.py
@@ -13,16 +13,14 @@ class random_actions:
         self.shape = (self.num_actions, 2)
 
     def get_action(self, obs):
-        #print('!!!!!!! Observation !!!!!!!!')
-        #print(obs)
-        #print(obs[0])
-        #for i in range(45,101,5):
-        #    print(obs[i:i+5])
-        #print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        
         action = np.zeros(self.shape)
         action[:, 0] = np.random.choice(self.num_groups, self.num_actions, replace=False)
         action[:, 1] = np.random.choice(np.arange(1, self.num_nodes + 1), self.num_actions, replace=False)
-        #print(action)
-        return action
+
+        # need to return Qs as well, just making zeros
+        Qs = np.zeros((132,))
+        
+        return action, Qs
 
 
