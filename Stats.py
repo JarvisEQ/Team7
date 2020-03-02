dbeabf9913958875b97b93401627af6bc646facb
JarvisEQ <lfjarvis@gmail.com>
2020-03-02 09:22:31 -0500

    fixing the stats

--- Stats.py
+++ Stats.py
@@ -1,25 +1,30 @@
 # Helps to view the battle statistics
 import numpy as np
+from collections import deque
+
+MEMORY_SIZE = 50
 
 class Stats:
     def __init__(self):
         self.numGames = 0
         self.numWins = 0
         self.winRate = 0
-        self.winHistory = []
+        self.history = deque(maxlen=MEMORY_SIZE)
 
     def updateStats(self, reward, game):
         self.numGames = game
-        self.winHistory.append(reward)
-
-        if reward == 1:
-            self.numWins = self.numWins + 1
+        self.history.append(reward)
         
-        self.winRate = np.round((self.numWins / self.numGames)*100, 2)
+        total = 0
+        for game in self.history:
+            if game == 1:
+                total += 1 
+        
+        self.winRate = total / MEMORY_SIZE
     
 
     def showWinRate(self):
-        print(f'Win Rate: {self.winRate}%')
+        print(f'Win Rate: {self.winRate}')
 
 
     # def plotWinRate():
