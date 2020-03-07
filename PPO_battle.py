2d205db37e36ee91c9ca58539e659e508841e867
Liam Jarvis <lfjarvis@gmail.com>
2020-03-07 12:54:29 -0500

    adding PPO and creating seperate battle files

---
+++ PPO_battle.py
@@ -0,0 +1,126 @@
+## Static Imports
+import os
+import importlib
+import gym
+import gym_everglades
+import pdb
+
+import numpy as np
+import random as r
+
+from Stats import Stats
+
+from everglades_server import server
+
+## Input Variables
+# Agent files must include a class of the same name with a 'get_action' function
+# Do not include './' in file path
+agent0_file = 'agents/PPO.py'
+#agent1_file = 'agents/same_commands.py'
+agent1_file = 'agents/random_actions.py'
+
+config_dir = './config/'
+map_file = config_dir + 'DemoMap.json'
+setup_file = config_dir + 'GameSetup.json'
+unit_file = config_dir + 'UnitDefinitions.json'
+output_dir = './game_telemetry/'
+
+# 0 for no debug /  1 for debug
+debug = 0
+
+view = 0
+
+createOut = 0
+
+numberOfGames = 500_000
+
+## Specific Imports
+agent0_name, agent0_extension = os.path.splitext(agent0_file)
+agent0_mod = importlib.import_module(agent0_name.replace('/','.'))
+agent0_class = getattr(agent0_mod, os.path.basename(agent0_name))
+
+agent1_name, agent1_extension = os.path.splitext(agent1_file)
+agent1_mod = importlib.import_module(agent1_name.replace('/','.'))
+agent1_class = getattr(agent1_mod, os.path.basename(agent1_name))
+
+## Main Script
+env = gym.make('everglades-v0')
+players = {}
+names = {}
+
+# Inputs for the dqn agent are:
+# state size, actions, player #, seed
+players[0] = agent0_class()
+names[0] = agent0_class.__name__
+
+players[1] = agent1_class(env.num_actions_per_turn, 1)
+names[1] = agent1_class.__name__
+
+# init stat class
+stats = Stats()
+
+
+for game in range(numberOfGames):
+    
+    # get inital state
+    current_state = env.reset(
+            players=players,
+            config_dir = config_dir,
+            map_file = map_file,
+            unit_file = unit_file,
+            output_dir = output_dir,
+            pnames = names,
+            debug = debug,
+            view = view,
+            out = createOut
+    )
+    new_state = current_state
+    
+    actions = {}
+    Qs = {}
+
+    # Game Loop
+    # assuming only training player 0
+    done = False
+    
+    while not done:
+        if debug:
+            env.game.debug_state()
+
+        if view:
+            env.game.view_state()
+        
+        # last new_state becomes current_state
+        current_state = new_state
+        
+        # get actions
+        for pid in players:
+            actions[pid], Qs[pid] = players[pid].get_action( current_state[pid] )
+        
+        # TODO, find out what info is?
+        new_state, reward, done, info = env.step(actions)
+ 
+        ### storing tranistions
+        players[0].update_replay_memory(current_state[0], new_state[0], Qs[0], reward[0], done)
+        
+        # uncomment here to add transition to opposing player here
+        # player[1].update_replay_memory(current_state[1], actions[1], reward[1], new_state[1], done)
+    
+    # trains only after game has finsihed
+    players[0].train(stats.getWinRate())
+    
+    # uncomment here to update opposing player here
+    # players[1].train()
+	
+	# updating the stats if needed
+    stats.updateStats(reward[0], game+1)
+    
+    # print-out for watching training
+    print(f"Game {game}")
+    stats.showWinRate()
+    players[0].get_debug()
+    print(f"reward = {reward}\n")
+
+# stat.plot
+# finally, save the model
+# model.saveModel()
---
+++ agents/PPO.py
@@ -0,0 +1,256 @@
+# PPO implementation 
+# Based on the half-rainbow implementation,
+# with Loss-cliping 
+
+
+# torch imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+# Other imports
+from .common.ReplayBuffer import ReplayBuffer
+import numpy as np
+import random 
+
+# Hyperparameters
+LEARNING_RATE = 0.000_3
+EPSILON = 0.99
+EPSILON_DECAY = 0.000_01
+TARGET_UPDATE = 10
+ACTION_SPACE = 132
+STATE_SPACE = 105
+REPLAY_MEMORY_SIZE = 50_000
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MINIBATCH_SIZE = 50
+DISCOUNT = 0.99
+
+# Unique to PPO, max and min of the loss
+LOSS_MIN = -1
+LOSS_MAX = 1
+
+
+PATH = "./agents/savedModels/rainbow/PPO.weights"
+
+
+class half_rainbow:
+    def __init__(self):
+        
+        # initalise device 
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        
+        # make main model
+        self.model = policy_network()
+        
+        # make target model
+        self.target_model = policy_network()
+        self.target_model.load_state_dict(self.model.state_dict())
+        
+        # send to device
+        self.model.to(self.device)
+        self.target_model.to(self.device)
+        
+        # loss and optimiser
+        self.opti = optim.Adam(self.model.parameters(), lr=LEARNING_RATE)
+        self.loss = torch.nn.MSELoss()
+        
+        # replay memory buffer
+        self.replay_memory = ReplayBuffer(STATE_SPACE, 
+                                          ACTION_SPACE, 
+                                          REPLAY_MEMORY_SIZE, 
+                                          MINIBATCH_SIZE)
+        
+        # some variables for later
+        # https://www.youtube.com/watch?v=a_Aej8hAVE4
+        self.name = "half_rainbow"
+        self.target_update_counter = 0
+        self.epsilon = EPSILON
+        self.win_rate = 0 
+        
+    # terminal_state is a bool, state is just the Qs
+    def train(self, win_rate):
+        
+        # test to make sure we have enought replay memory to do the training
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return 
+        
+        sample = self.replay_memory.get_sample()
+        
+        # move sample to device for increase speed
+        state = torch.FloatTensor(sample["init_state"]).to(self.device)
+        next_state = torch.FloatTensor(sample["next_state"]).to(self.device)
+        action = torch.LongTensor(sample["action"].reshape(-1, 1)).to(self.device)
+        reward = torch.FloatTensor(sample["reward"].reshape(-1, 1)).to(self.device)
+        done = torch.FloatTensor(sample["done"].reshape(-1, 1)).to(self.device)
+
+        # will be generated from our sample
+        current_qs = torch.zeros([50,132]).to(self.device)
+        expected_qs = torch.zeros([50,132]).to(self.device)
+        
+        # find the Qs and send to our device
+        for i in range(MINIBATCH_SIZE):
+            
+            current_qs[i] = self.model.forward(state[i])
+            
+            expected_qs[i] = self.target_model(next_state[i])
+        
+        mask = 1 - done
+        target = (reward + DISCOUNT * expected_qs * mask).to(self.device)
+        
+        
+        # do optimise on the minibatch
+        self.opti.zero_grad()
+        
+        # When calculating the loss, use clamp to clip it
+        # this is how we get the proximal policy optimisation to work!
+        loss = self.loss(current_qs, target)
+        loss = torch.clamp(loss, min=-LOSS_MIN, max=LOSS_MAX)
+        loss.backward()
+        self.opti.step()
+        
+        # check to see if we need to update the target_model
+        self.target_update_counter += 1
+        
+        # updating the target model if it's time
+        if self.target_update_counter > TARGET_UPDATE:
+            self.target_model.load_state_dict(self.model.state_dict())
+            self.target_update_counter = 0
+            
+        # only save the model if it's the best model
+        if win_rate > self.win_rate:
+            self.saveModel()
+            self.win_rate = win_rate
+            
+        # decay that epsilon
+        self.epsilon -= EPSILON_DECAY
+        
+        
+    def get_action(self, state):
+        
+        # move state from np to Tensor and to cuda
+        state = torch.Tensor(state)
+        state = state.to(self.device)
+        
+        # get Qs and turn into np array
+        Qs = self.model(state)
+        Qs = Qs.cpu()
+        Qs = Qs.data.numpy()
+        
+        # epsilon-greedy policy 
+		# TODO, remove when noisy is implemented
+        if random.random() < self.epsilon:
+            
+            # random actions
+            # hard-coding these in, epsilon-greedy is going to be undone in future rainbow version
+            actions = np.zeros((7, 2))
+            actions[:, 0] = np.random.choice(12, 7, replace=False)
+            actions[:, 1] = np.random.choice(11, 7, replace=False)
+        else:
+            
+            # translate the Qs to action pairs
+            actions = self.translateQs(Qs)
+        
+        # make transition
+        
+        return actions, Qs
+    
+    def update_replay_memory(self, 
+                            init_state,
+                            next_state,
+                            action,
+                            reward,
+                            done):
+        
+        self.replay_memory.store_transition(init_state,
+                                            next_state,
+                                            action,
+                                            reward,
+                                            done)
+    
+    # expects a numpy array of size 72 and state of size 105
+    def translateQs(self, Qs):
+    
+        actionArray = []
+        units = []	
+        
+        # reshaping the array makes life easier
+        Qs = np.reshape(Qs, (12,11))
+
+        while len(actionArray) < 7 :
+        
+            # get the max Q
+            action = np.unravel_index(np.argmax(Qs, axis=None), Qs.shape)
+            
+            # set action low so that we do not chose it again
+            Qs[action] = float('-inf')
+            
+            # convert from a tuple to a np array
+            action = np.array(action)
+            
+
+            # check to see if the unit is already being moved
+            if action[0] in units:
+                continue	
+            
+            # add the unit to the unit chosen array
+            units.append(action[0])		
+
+            # append it to the action pair
+            actionArray.append(action)
+        
+        # return the array
+        return np.array(actionArray)
+        
+    # pretty self explanitory 
+    def saveModel(self):
+        torch.save({"model_weights": self.model.state_dict(), 
+                   "win_rate": self.win_rate,
+                   "epsilon": self.epsilon},
+                   PATH)
+    
+    # grab a checkout weights, used mostly in testing 
+    def load_model(self):
+        
+        checkpoint = torch.load(PATH)
+        
+        self.model.load_state_dict(checkpoint["model_weights"])
+        self.target_model.load_state_dict(checkpoint["model_weights"])
+        self.epsilon = checkpoint["epsilon"]
+        self.win_rate = checkpoint["win_rate"]
+        
+        self.get_debug()
+        
+        return
+    
+    # this prints out helpful information for training
+    def get_debug(self):
+        print(f"epsilon = {self.epsilon}")
+        print(f"best win rate = {self.win_rate}")
+        return
+        
+# our magical network 
+# TODO, probably need to make deeper and thiccer for betting results!
+class policy_network(nn.Module):
+    
+    def __init__(self):
+        super(policy_network, self).__init__()
+        
+        # fc layers to outputs
+        self.fc0 = nn.Linear(STATE_SPACE, 2048)
+        self.fc1 = nn.Linear(2048, 2048)
+        self.fc2 = nn.Linear(2048, 2048)
+        self.fc3 = nn.Linear(2048, ACTION_SPACE)
+        
+    def forward(self, x):
+        
+        x = torch.flatten(x)
+        x = F.relu(self.fc0(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        
+        # softmax for outputs, need
+        x = F.softmax(self.fc3(x), dim=-1)
+        return x  
+    
